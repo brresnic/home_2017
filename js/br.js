2266ac1543ec676b6fb4cf2cce698f2f60f7c51d
BENJAMIN R. RESNICK <brresnic@us.ibm.com>
2017-08-29 16:04:03 -0500

    website layout

--- js/br.js
+++ js/br.js
@@ -1,26 +1,26 @@
+function adjustMenuItems() {
+  var position = $(this).scrollTop();
+  // console.log('position ' + position);
+
+  $('.section').each(function() {
+      var target = $(this).offset().top;
+      var id = $(this).attr('id');
+
+      if (position + 130 >= target) {
+          console.log(id);
+          $('#sticky-nav > a').removeClass('active');
+          $('#sticky-nav > a[href="#' + id + '"]').addClass('active');
+      }
+  });
+}
+
 $( document ).ready(function() {
-  $('#yl').hover(function() {
-      $('#ylh').fadeIn('400');
-  }, function() {
-    if($( document ).width() > 729){    
-      $('#ylh').fadeOut('400');
-    }
+  $(window).scroll(function() {
+    adjustMenuItems();
   });
 
-  $('#mm').hover(function() {
-      $('#mmh').fadeIn('400');
-  }, function() {
-    if($( document ).width() > 729){
-      $('#mmh').fadeOut('400');
-    }
-  });
+});
 
-  $('#h2').hover(function() {
-      $('#h2h').fadeIn('400');
-  }, function() {
-    if($( document ).width() > 729){
-      $('#h2h').fadeOut('400');
-    }
-  });
-
+window.addEventListener("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY - 50);
 });
